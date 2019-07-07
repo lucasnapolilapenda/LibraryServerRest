7d828c9963156569f03e2a852417730dca8ed240
Lucas Napoli <lucasnapoli@wattopoststudio.com>
2019-07-07 17:45:54 -0400

    Initial Commit. Project Set

---
+++ src/main/java/com/lucas/rest/json/Book.java
@@ -0,0 +1,12 @@
+package com.lucas.rest.json;
+
+public class Book {
+
+    private int id;
+    private String title;
+    private String description;
+    private String isbn;
+    private String author;
+    private String publisher;
+
+}
