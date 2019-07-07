66085bf3241bd83c76487573a000549c6b799a93
Lucas Napoli <lucasnapoli@wattopoststudio.com>
2019-07-07 17:57:42 -0400

    Book Entity Created Adding Repository

--- src/main/java/com/lucas/rest/json/Book.java
+++ src/main/java/com/lucas/rest/json/Book.java
@@ -1,6 +1,6 @@
 package com.lucas.rest.json;
 
-public class Book {
+public class Book implements Cloneable {
 
     private int id;
     private String title;
@@ -9,4 +9,62 @@ public class Book {
     private String author;
     private String publisher;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Book clone() {
+        Book b = new Book();
+        b.setId(this.id);
+        b.setTitle(this.title);
+        b.setDescription(this.description);
+        b.setIsbn(this.isbn);
+        b.setAuthor(this.author);
+        b.setPublisher(this.publisher);
+        return b;
+    }
 }
---
+++ src/main/java/com/lucas/rest/json/Repository.java
@@ -0,0 +1,4 @@
+package com.lucas.rest.json;
+
+public class Repository {
+}
